8be4d2773cbd8d1c93cd6e87973bdd30f921608d
Oleg NN <zvoprosno@gmail.con>
2023-02-12 15:34:03 +0300

    changed access rights

--- src/atomic/Main.java
+++ src/atomic/Main.java
@@ -5,16 +5,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    public static AtomicInteger counter3 = new AtomicInteger(0);
-    public static AtomicInteger counter4 = new AtomicInteger(0);
-    public static AtomicInteger counter5 = new AtomicInteger(0);
-    public static short SIMILAR = 1;
-    public static short BY_HEIGHT = 2;
+    static AtomicInteger counter3 = new AtomicInteger(0);
+    static AtomicInteger counter4 = new AtomicInteger(0);
+    static AtomicInteger counter5 = new AtomicInteger(0);
+    static short SIMILAR = 1;
+    static short BY_HEIGHT = 2;
     public static final int masSize = 100_000;
-    public static final int stringSizePlus = 3;
-    public static AtomicInteger stop = new AtomicInteger(0);
+    static final int stringSizePlus = 3;
+    static AtomicInteger stop = new AtomicInteger(0);
     ;
-    public static final String symbol = "abc";
+    static final String symbol = "abc";
 
     static String[] texts = new String[masSize];
 
@@ -37,7 +37,7 @@ public class Main {
         System.out.println("Красивых слов с длиной 5: " + counter5 + " шт.");
     }
 
-    public static String generateText(String letters, int length) {
+    static String generateText(String letters, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
         for (int i = 0; i < length; i++) {
@@ -46,11 +46,11 @@ public class Main {
         return text.toString();
     }
 
-    public static String reverse(String s) {
+    static String reverse(String s) {
         return new StringBuilder(s).reverse().toString();
     }
 
-    public static short byHeight(String s) {
+    static short byHeight(String s) {
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) != s.charAt(0)) {
                 for (int j = 0; j < s.length() - 1; j++) {
@@ -65,7 +65,7 @@ public class Main {
         return SIMILAR;
     }
 
-    public static void adder(int length) {
+    static void adder(int length) {
         if (length == 3) {
             counter3.set(counter3.get() + 1);
         } else if (length == 4) {
